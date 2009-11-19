1cda5a51792d8daed2d62b3ae53866f05e04faa6
dakota <wlalk@cmscout.co.za>
2009-11-19 10:13:40 +0200

    Removed PHP5.3 only function

--- controllers/components/event.php
+++ controllers/components/event.php
@@ -186,7 +186,8 @@ class EventComponent extends Object
 		{
 			if(strpos($availableMethod, 'on') === 0)
 			{
-				$handlerName = lcfirst(substr($availableMethod, 2));
+				$handlerName = substr($availableMethod, 2);
+				$handlerName{0} = strtolower($string{0});
 				$this->eventHandlerCache[$handlerName][] = get_class($Event);
 			}
 		}
